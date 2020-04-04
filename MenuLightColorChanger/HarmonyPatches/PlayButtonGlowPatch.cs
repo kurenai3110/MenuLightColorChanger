7366c18381f8e4c9efe9393bfc9fd7efe8057a30
Kevin Kylon <kylon99@gmail.com>
2020-04-04 00:07:07 -0700

    Updated to support Beat Saber 1.8.0 and BSIPA 4.0.

--- MenuLightColorChanger/HarmonyPatches/PlayButtonGlowPatch.cs
+++ MenuLightColorChanger/HarmonyPatches/PlayButtonGlowPatch.cs
@@ -1,10 +1,5 @@
 ﻿using BS_Utils.Utilities;
-using Harmony;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using HarmonyLib;
 using UnityEngine;
 using UnityEngine.UI;
 
